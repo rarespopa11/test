2708474d08ac83c36d43a037a756763d2b4e4115
rarespopa11 <raresnicholas@yahoo.com>
2025-05-14 15:49:14 +0300

    cerinte

--- problema_1.c
+++
@@ -1,5 +0,0 @@
-/**
-Varianta 17 - Problema 1
-1.	Cum sunt reprezentate si stocate argumentele de pe linia de comanda ? Dar variabilele de mediu transmise unui program C ? Mentionati valorile argc, argv si envp pentru urmatoarea linie:
-export X=2; export Y=3; ./a.out -a 2 -b 3
-*/
